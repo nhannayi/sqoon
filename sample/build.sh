2d4ab776df2b33346fe0d0e8a3fb2657b73862cf
evancyrist <121801903+evancyrist@users.noreply.github.com>
2024-10-27 11:48:02 +0800

    Update build.sh

--- sample/build.sh
+++ sample/build.sh
@@ -11,3 +11,14 @@ TERMUX_PKG_SRCURL=
 TERMUX_PKG_SHA256=
 #TERMUX_PKG_DEPENDS=""
 #TERMUX_PKG_BUILD_IN_SRC=true
+ermux_step_make_install() {
+	install -Dm0755 \
+		-t "$TERMUX_PREFIX"/bin \
+		"$TERMUX_PKG_BUILDDIR"/CPP/7zip/Bundles/Alone2/b/c/7zz
+	install -Dm0644 \
+		-t "$TERMUX_PREFIX"/share/doc/"$TERMUX_PKG_NAME" \
+		"$TERMUX_PKG_BUILDDIR"/DOC/{7zC,7zFormat,lzma,Methods,readme,src-history}.txt
+	install -Dm0644 \
+		-t "$TERMUX_PREFIX"/share/LICENSES/"$TERMUX_PKG_NAME" \
+		"$TERMUX_PKG_BUILDDIR"/DOC/{copying,License}.txt
+}
